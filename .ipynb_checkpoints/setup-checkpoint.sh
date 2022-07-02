2b49d2736ec80beb70300fb0a7ab4ee42936f5a6
Tianyu Liu <tianyu.liu@inf.ethz.ch>
2022-07-02 12:01:32 +0200

    logbmmsq

--- .ipynb_checkpoints/setup-checkpoint.sh
+++
@@ -1,3 +0,0 @@
-apt-get install ccache
-pip3 install hypothesis ninja
-export CXX=/usr/lib/ccache/c++
--- genbmm/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,4 +0,0 @@
-from .genmul import logbmm, samplebmm, maxbmm, prodmaxbmm, logbmmsq
-from .sparse import BandedMatrix, banddiag
-
-__all__ = [logbmm, logbmmsq, samplebmm, maxbmm, prodmaxbmm, BandedMatrix, banddiag]
