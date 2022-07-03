a825f616d87206665ef90a9dfa37d726ec8caa04
Tianyu Liu <tianyu.liu@inf.ethz.ch>
2022-07-04 01:41:11 +0200

    init

--- genbmm/__init__.py
+++ genbmm/__init__.py
@@ -1,4 +1,4 @@
-from .genmul import logbmm, samplebmm, maxbmm, prodmaxbmm, logbmmsq
+from .genmul import logbmm, samplebmm, maxbmm, prodmaxbmm, logbmminside
 from .sparse import BandedMatrix, banddiag
 
-__all__ = [logbmm, logbmmsq, samplebmm, maxbmm, prodmaxbmm, BandedMatrix, banddiag]
+__all__ = [logbmm, logbmminside, samplebmm, maxbmm, prodmaxbmm, BandedMatrix, banddiag]
